e6ab6c1589b71b8aed9dd4a972159d029fc51271
HennadiiMariiev <marieff1990@gmail.com>
2023-05-25 14:38:29 +0300

    Revert "unused"
    This reverts commit f3240b0111704fa9566cb5ab23686307450fa71d.

--- src/config/bot_constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MAGIC_STICKER_ID = void 0;
-const MAGIC_STICKER_ID = "CgACAgQAAxkBAAIChmQkInYaNPGzJ6u4_B3xH-fa4-_RAAIOAwACl2MEU3-lr3Md1yF1LwQ";
-exports.MAGIC_STICKER_ID = MAGIC_STICKER_ID;
